61d0432fc2eb9972533b8a1ee51da335a3ec9330
JamesHsu1221 <love98505732@gmail.com>
2024-09-17 20:35:03 +0800

    完成B小題

--- homework1.m
+++ homework1.m
@@ -1,16 +1,25 @@
 load("HW1_brain.mat");
 
-%image test
+%image test 
 %origin_image=mat2gray(HW1_brain);
 %imshow(origin_image);
 
 %question A
 
-level = 8;    % intensity level setting（1~8）
-intensity_level = power(2,level);
+bits = 8;    % intensity level setting（1~8）
+
+intensity_level = power(2,bits);
 max_num = max(HW1_brain(:))+0.001;    %加0.001是為了不要讓最大的那一個數變額外的整數
 gap = (max_num/intensity_level);    %作為取樣間隔
 adjust_image = floor(HW1_brain/gap);
 final_image = mat2gray(adjust_image);
+figure(1);
 imshow(final_image);
+
+%question B (question A's bits must be 8)
+MSB_8bits = bitand(adjust_image,128);
+figure(2);
+imshow(mat2gray(MSB_8bits));
+
+
 
