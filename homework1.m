aac9a36553913b0b97f72438d1e792f3cc699075
JamesHsu1221 <love98505732@gmail.com>
2024-09-17 21:54:06 +0800

    更改B小題的作法&完成C小題

--- homework1.m
+++ homework1.m
@@ -12,15 +12,19 @@ intensity_level = power(2,bits);
 max_num = max(HW1_brain(:))+0.001;    %加0.001是為了不要讓最大的那一個數變額外的整數
 gap = (max_num/intensity_level);    %作為取樣間隔
 adjust_image = floor(HW1_brain/gap);
-final_image = mat2gray(adjust_image);
+final_image = mat2gray(adjust_image); %將最後的數值歸一化
 figure(1);
 imshow(final_image);
 
 %question B (line 21 is question A's code but always 8bits)
 
-adjust_image_B = floor(HW1_brain/(max_num/256)); % merge line 11 to line 14
+adjust_image_B = uint8(floor(HW1_brain/(max_num/256))); % merge line 11 to line 14 and change it to int8bit
 MSB_8bits = bitand(adjust_image_B,128);
 figure(2);
-imshow(mat2gray(MSB_8bits));
+imshow(MSB_8bits);
 
+%question C
+MSB_8bits_0 = bitand(adjust_image_B,127);
+figure(3);
+imshow(MSB_8bits_0);
 
