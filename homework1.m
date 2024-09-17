305ad008ed0be8483bf3c4e15c49c960bddfb530
JamesHsu1221 <love98505732@gmail.com>
2024-09-17 22:06:42 +0800

    完成D小題

--- homework1.m
+++ homework1.m
@@ -12,19 +12,25 @@ intensity_level = power(2,bits);
 max_num = max(HW1_brain(:))+0.001;    %加0.001是為了不要讓最大的那一個數變額外的整數
 gap = (max_num/intensity_level);    %作為取樣間隔
 adjust_image = floor(HW1_brain/gap);
-final_image = mat2gray(adjust_image); %將最後的數值歸一化
+final_image = mat2gray(adjust_image);    %將最後的數值歸一化
 figure(1);
 imshow(final_image);
 
 %question B (line 21 is question A's code but always 8bits)
 
-adjust_image_B = uint8(floor(HW1_brain/(max_num/256))); % merge line 11 to line 14 and change it to int8bit
-MSB_8bits = bitand(adjust_image_B,128);
+adjust_image_8bits = uint8(floor(HW1_brain/(max_num/256)));    % merge line 11 to line 14 and change it to int8bit
+MSB_8bits = bitand(adjust_image_8bits,128);      %make and with 1000 0000
 figure(2);
 imshow(MSB_8bits);
 
 %question C
-MSB_8bits_0 = bitand(adjust_image_B,127);
+
+MSB_8bits_0 = bitand(adjust_image_8bits,127);    %make and with 0111 1111
 figure(3);
 imshow(MSB_8bits_0);
 
+%question D
+
+LSB_8bits_0 = bitand(adjust_image_8bits,254);    %make and with 1111 1110
+figure(4);
+imshow(LSB_8bits_0);
